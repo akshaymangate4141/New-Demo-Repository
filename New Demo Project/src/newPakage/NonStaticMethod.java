e63486243b73a7ec6d86d7fb4aa8617749ad22f7
akshaymangate4141 <104920245+akshaymangate4141@users.noreply.github.com>
2022-06-17 16:37:57 +0530

    Created A Non Static Class
    I Have Created A Non Static Class Please Review It..

---
+++ New Demo Project/src/newPakage/NonStaticMethod.java
@@ -0,0 +1,34 @@
+package newPakage;
+
+public class NonStaticMethod {
+
+	public static void main(String args [])
+	{
+		
+		
+		
+		
+		
+	}
+	
+	public static void method1()
+	{
+		System.out.println("This Is My Static Method1");
+	}
+	
+	public void method2()
+	{
+		System.out.println("This Is My Non Static Method");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
